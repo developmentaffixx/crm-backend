0a90f92be0a2d74d892db12229922e23fe5635bf
developmentaffixx <developmentaffixx@gmail.com>
2026-06-12 12:40:39 +0530

    time

--- src/config/db.js
+++ src/config/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   user:     process.env.DB_USER     || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME     || 'crm_task_module',
+  timezone: '+05:30',
   waitForConnections: true,
   connectionLimit:    10,
   queueLimit:         0,
