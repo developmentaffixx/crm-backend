5485ba9ca4662c5e031be2bf14636a9babae0e71
developmentaffixx <developmentaffixx@gmail.com>
2026-08-18 15:37:55 +0530

    SMM check

--- database/migration_allocation_deliverables_update.sql
+++ database/migration_allocation_deliverables_update.sql
@@ -2,6 +2,7 @@
 -- 1. Change commitment_stories, commitment_content_calendar, commitment_insight_report, commitment_strategy_call from INT to VARCHAR (dropdown values)
 -- 2. Change shoot_sessions to VARCHAR (dropdown: Main Session / Patch Session)
 -- 3. Add ads_pre_ad_report and ads_post_ad_report columns
+-- 4. Remove old ads columns (campaign_setup, campaign_monitoring, optimization, reporting)
 
 ALTER TABLE project_allocation_sheets
   MODIFY COLUMN commitment_stories VARCHAR(50) DEFAULT NULL,
@@ -14,3 +15,9 @@ ALTER TABLE project_allocation_sheets
 ALTER TABLE project_allocation_sheets
   ADD COLUMN ads_pre_ad_report TINYINT(1) DEFAULT 0 AFTER ads_reporting,
   ADD COLUMN ads_post_ad_report TINYINT(1) DEFAULT 0 AFTER ads_pre_ad_report;
+
+ALTER TABLE project_allocation_sheets
+  DROP COLUMN ads_campaign_setup,
+  DROP COLUMN ads_campaign_monitoring,
+  DROP COLUMN ads_optimization,
+  DROP COLUMN ads_reporting;
